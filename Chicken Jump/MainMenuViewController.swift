5f39d991c7062ad213949f6c33049c67f493ba74
Gilberd Siboro <gsiboro11016@gmail.com>
2024-07-01 16:17:30 +0700

    Initial Commit

---
+++ Chicken Jump/MainMenuViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainMenuViewController.swift
+//  Chicken Jump
+//
+//  Created by Foundation-010 on 27/06/24.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+//    override var prefersStatusBarHidden: Bool {
+//        return true
+//    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
