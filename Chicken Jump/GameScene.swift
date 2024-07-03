ddeb7ebbdbaee3971f61dc4fec79393581a18662
Gilberd Siboro <gsiboro11016@gmail.com>
2024-07-03 15:44:57 +0700

    obstacle + game over

--- Chicken Jump/GameScene.swift
+++ Chicken Jump/GameScene.swift
@@ -15,61 +15,82 @@ class GameScene: SKScene {
     
     var pijakan: SKSpriteNode?
     var obstacle1: SKSpriteNode?
-    var hpLabel: SKLabelNode?
-
     var obstacle2: SKSpriteNode?
+    var hpLabel: SKLabelNode?
     var xPosition = [-150, 150]
     var hp = "❤️❤️❤️"
+    var obstacle1Exist = false
+    var obstacles: [SKSpriteNode?] = []
+
     
     override func didMove(to view: SKView) {
         pijakan = childNode(withName: "//pijakan") as? SKSpriteNode
+        
         obstacle1 = childNode(withName: "//obstacle1") as? SKSpriteNode
         obstacle2 = childNode(withName: "//obstacle2") as? SKSpriteNode
         
-        repeatedlySpawnPijakan()
-        repeatedlySpawnObstacle1()
-        repeatedlySpawnObstacle2()
+        obstacles.append(pijakan)
+        obstacles.append(pijakan)
+        obstacles.append(pijakan)
+        obstacles.append(obstacle1)
+        obstacles.append(obstacle2)
+        
+        
+        repeatedlySpawnObstacle()
     }
+
     
-    func repeatedlySpawnPijakan() {
+
+    
+    func repeatedlySpawnObstacle() {
         let spawnAction = SKAction.run {
-            self.spawnPijakan()
+            self.spawnObstacles()
         }
         let waitAction = SKAction.wait(forDuration: 2)
         let spawnAndWaitAction = SKAction.sequence([spawnAction, waitAction])
         run(SKAction.repeatForever(spawnAndWaitAction))
     }
     
-    func spawnPijakan() {
-        if let newPijakan = pijakan?.copy() as? SKSpriteNode {
-            newPijakan.position = CGPoint(x: xPosition[Int.random(in: 0...1)], y: 700)
-            newPijakan.physicsBody = SKPhysicsBody(rectangleOf: newPijakan.size)
-            newPijakan.physicsBody?.isDynamic = false
-            
-            addChild(newPijakan)
-            
-            movePijakan(node: newPijakan)
-        }
-    }
+    var lastXPositionOfObstacle = 150
     
-    func movePijakan(node: SKNode) {
-        let moveDownAction = SKAction.moveTo(y: -700, duration: 5)
-        let removeNodeAction = SKAction.removeFromParent()
-        node.run(SKAction.sequence([moveDownAction, removeNodeAction]))
-    }
-    
-    func repeatedlySpawnObstacle1() {
-        let spawnAction = SKAction.run {
-            self.spawnObstacle1()
+    func spawnObstacles() {
+        var randomObstacle = obstacles.randomElement() ?? obstacle1
+        
+        while (obstacle1Exist == true) {
+            randomObstacle = obstacles.randomElement() ?? obstacle1
+            
+            if randomObstacle != obstacle1 {
+                obstacle1Exist = false
+            }
         }
-        let waitAction = SKAction.wait(forDuration: 3)
-        let spawnAndWaitAction = SKAction.sequence([spawnAction, waitAction])
-        run(SKAction.repeatForever(spawnAndWaitAction))
+        
+        if let newObstacle = randomObstacle?.copy() as? SKSpriteNode {
+            
+            if lastXPositionOfObstacle == 150 {
+                lastXPositionOfObstacle = -150
+            } else {
+                lastXPositionOfObstacle = 150
+            }
+            newObstacle.position = CGPoint(x: lastXPositionOfObstacle, y: 1000)
+            newObstacle.physicsBody = SKPhysicsBody(rectangleOf: newObstacle.size)
+            newObstacle.physicsBody?.isDynamic = false
+            
+            addChild(newObstacle)
+            
+            moveObstacle1(node: newObstacle)
+        }
+        
+    
+        if randomObstacle == obstacle1 {
+            obstacle1Exist = true
+        }
+        
     }
     
     func spawnObstacle1() {
         if let newObstacle1 = obstacle1?.copy() as? SKSpriteNode {
-            newObstacle1.position = CGPoint(x: xPosition[Int.random(in: 0...1)], y: 700)
+            
+            newObstacle1.position = CGPoint(x: xPosition[Int.random(in: 0...1)], y: 1000)
             newObstacle1.physicsBody = SKPhysicsBody(rectangleOf: newObstacle1.size)
             newObstacle1.physicsBody?.isDynamic = false
             
@@ -80,37 +101,11 @@ class GameScene: SKScene {
     }
     
     func moveObstacle1(node: SKNode) {
-        let moveDownAction = SKAction.moveTo(y: -700, duration: 6)
+        let moveDownAction = SKAction.moveTo(y: -800, duration: 15)
         let removeNodeAction = SKAction.removeFromParent()
         node.run(SKAction.sequence([moveDownAction, removeNodeAction]))
     }
-    
-    func repeatedlySpawnObstacle2() {
-        let spawnAction = SKAction.run {
-            self.spawnObstacle2()
-        }
-        let waitAction = SKAction.wait(forDuration: 4)
-        let spawnAndWaitAction = SKAction.sequence([spawnAction, waitAction])
-        run(SKAction.repeatForever(spawnAndWaitAction))
-    }
-    
-    func spawnObstacle2() {
-        if let newObstacle2 = obstacle2?.copy() as? SKSpriteNode {
-            newObstacle2.position = CGPoint(x: xPosition[Int.random(in: 0...1)], y: 700)
-            newObstacle2.physicsBody = SKPhysicsBody(rectangleOf: newObstacle2.size)
-            newObstacle2.physicsBody?.isDynamic = false
-            
-            addChild(newObstacle2)
-            
-            moveObstacle2(node: newObstacle2)
-        }
-    }
-    
-    func moveObstacle2(node: SKNode) {
-        let moveDownAction = SKAction.moveTo(y: -700, duration: 5)
-        let removeNodeAction = SKAction.removeFromParent()
-        node.run(SKAction.sequence([moveDownAction, removeNodeAction]))
-    }
+
     
     func didBegin(_ contact: SKPhysicsContact){
         guard let nodeA = contact.bodyA.node else { return }
